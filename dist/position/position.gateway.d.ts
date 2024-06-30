a49c37c78d236ecefecf19f3e73ebaee1d6f7580
yoo20370 <email@email.com>
2024-06-30 14:30:45 +0900

    make connect, disconnect method

--- dist/position/position.gateway.d.ts
+++ dist/position/position.gateway.d.ts
@@ -1,8 +1,11 @@
+import { OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
 import { Server } from 'http';
-export declare class PositionGateway {
+export declare class PositionGateway implements OnGatewayConnection, OnGatewayDisconnect {
     server: Server;
     wsClients: any[];
     userlocations: {};
+    handleConnection(client: any): void;
+    handleDisconnect(client: any): void;
     connectSomeone(data: string, client: any): void;
     disconnectClient(client: any): void;
     private broadcast;
--- src/position/position.gateway.ts
+++ src/position/position.gateway.ts
@@ -1,6 +1,8 @@
 import {
   ConnectedSocket,
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
@@ -12,7 +14,7 @@ import { Server } from 'http';
   namespace: 'quizly',
   cors: { origin: '*' },
 })
-export class PositionGateway {
+export class PositionGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
 
@@ -21,14 +23,33 @@ export class PositionGateway {
 
   // 접속 중인 유저 위치 정보 ( nickname : {x,y,z})
   userlocations = {};
-  
+
   /*
+    handleConnection 메서드
+    클라이언트가 서버에 접속하면 실행되는 메서드
+  */
+  handleConnection(client) {
+    console.log(`Client connected: ${client.id}`);
+  }
+
+  /*
+    handleDisconnect 메서드
+    클라이언트가 서버에서 접속을 끊으면 실행되는 메서드  
+  */
+  handleDisconnect(client) {
+    console.log(`Client disconnected: ${client.id}`);
+    // 웹 소켓에서 나갈 떄 유저 목록에서 제거 
+    this.wsClients = this.wsClients.filter(c => c.id !== client.id);
+  }
+
+  /*
+    connectSomeone 메서드
     클라이언트가 room 이벤트를 발생시키면 서버는 connectSomeone 메소드를 실행한다.
   */
   //TODO: room을 roomIN으로 변경해줄 필요가 있다.
   @SubscribeMessage('room')
-  connectSomeone(@MessageBody() data: string, @ConnectedSocket() client){
-    
+  connectSomeone(@MessageBody() data: string, @ConnectedSocket() client) {
+
     //TODO: 쿠키로 사용자를 구분할 필요가 있음 !!
     // 이미 접속한 사용자인지 확인
     const isAlreadyConnected = this.wsClients.some((curr) => {
@@ -52,8 +73,8 @@ export class PositionGateway {
     this.wsClients.push(client);
 
     // 입장한 유저의 초기 위치 
-    const initialValue = { x : 0, y : 0, z : 0 };
-    
+    const initialValue = { x: 0, y: 0, z: 0 };
+
     this.userlocations[nickname] = initialValue;
 
     /*
@@ -62,7 +83,7 @@ export class PositionGateway {
     */
     client.emit("roomIn", this.userlocations);
   }
-
+  // disconnectClient 메서드 
   //TODO: out을 roomOUT으로 변경해줄 필요가 있다.
   @SubscribeMessage('out')
   disconnectClient(client) {
@@ -82,13 +103,14 @@ export class PositionGateway {
   }
 
   /*
+    sendMessage 메서드  
     사용자가 움직일 때마다 send 이벤트를 발생시킨다.'
     그 때 해당 메서드가 실행되어 모든 클라이언트에게 사용자 위치를 브로드케스트한다.
-  */ 
+  */
   @SubscribeMessage('send')
   sendMessage(@MessageBody() data: string, @ConnectedSocket() client) {
     const [room, nickname, message] = data;
-  
+
     console.log("message", message, client.id);
 
     this.userlocations[nickname] = message;
