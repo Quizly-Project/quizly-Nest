36931219a0458e4cf479455df6c4af2b31f85cc2
jjr7181 <jjr7181@gmail.com>
2024-07-05 19:33:20 +0900

    feat: 닉네임 받도록 설정, 채팅 구현 완료

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,6 +1,8 @@
 import { MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import { Socket, Server } from "socket.io";
 import { IsNotEmpty, IsString } from "class-validator";
+import { UseFilters, UsePipes, ValidationPipe } from "@nestjs/common";
+import { WebsocketExceptionFilter } from "./ws-exception.filter";
 
 class ChatMessage {
 
@@ -14,6 +16,7 @@ class ChatMessage {
 }
 
 @WebSocketGateway (3002, { cors: { origin: '*' } }) //default는 main의 3000 port
+@UseFilters(new WebsocketExceptionFilter())
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer() server: Server;
 
@@ -34,55 +37,11 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   }
 
   @SubscribeMessage('newMessage')
-  handleNewMessage(@MessageBody() message: String) {
-    this.server.emit('message', message);
+  @UsePipes (new ValidationPipe())
+  handleNewMessage(@MessageBody() message: ChatMessage) {
+    this.server.emit('message', {
+      ...message,
+      time: new Date().toDateString(),
+    });
   }
 }
-
-// import { MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
-// import { Socket, Server } from "socket.io";
-
-// @WebSocketGateway(3002, { cors: { origin: '*' } })
-// export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
-//   @WebSocketServer() server: Server;
-
-//   private userColors: Map<string, string> = new Map();
-
-//   private getColorForNickname(nickname: string): string {
-//     if (!this.userColors.has(nickname)) {
-//       const color = '#' + Math.floor(Math.random()*16777215).toString(16);
-//       this.userColors.set(nickname, color);
-//     }
-//     return this.userColors.get(nickname);
-//   }
-
-//   handleConnection(client: Socket) {
-//     console.log('New user connected..', client.id);
-//     this.server.emit('user-joined', {
-//       message: `New User Joined the Chat: ${client.id}`,
-//     });
-//   }
-
-//   handleDisconnect(client: Socket) {
-//     console.log('user disconnected..', client.id);
-//     this.server.emit('user-left', {
-//       message: `User Left the Chat: ${client.id} `,
-//     });
-//   }
-
-//   @SubscribeMessage('setNickname')
-//   handleSetNickname(client: Socket, nickname: string) {
-//     const color = this.getColorForNickname(nickname);
-//     client.emit('nicknameColor', { nickname, color });
-//   }
-
-//   @SubscribeMessage('newMessage')
-//   handleNewMessage(@MessageBody() data: { nickname: string, message: string }) {
-//     const color = this.getColorForNickname(data.nickname);
-//     this.server.emit('message', { 
-//       nickname: data.nickname, 
-//       message: data.message, 
-//       color: color 
-//     });
-//   }
-// }
