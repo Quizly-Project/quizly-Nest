1fd3c7ca8dc757098b1ae36744beec97a8d8efca
yoo20370 <email@email.com>
2024-07-20 13:45:37 +0900

    feat: 퀴즈 타이머 시간 1.5초 늘림

--- src/play/play.service.ts
+++ src/play/play.service.ts
@@ -249,10 +249,13 @@ export class PlayService {
     console.log(duration, room.roomCode);
 
     this.stopQuizTimer(room.roomCode);
-    const timer = setTimeout(() => {
-      // 타임아웃 처리
-      this.handleTimeout(room, server, room.quizGroup.quizzes[0].type);
-    }, duration * 1000);
+    const timer = setTimeout(
+      () => {
+        // 타임아웃 처리
+        this.handleTimeout(room, server, room.quizGroup.quizzes[0].type);
+      },
+      duration * 1000 + 1500
+    );
 
     this.timers.set(room.roomCode, timer);
 
